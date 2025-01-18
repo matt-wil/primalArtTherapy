a3aec0375d3b9bd046a4fabf2f18d9e749f7d957
Matt Williams <matthew.steven.williams@gmail.com>
2025-01-18 16:56:51 +0100

    boiler plate code added

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow
 
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+
+def main():
+    app = QApplication(sys.argv)  # allows CLI args
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
